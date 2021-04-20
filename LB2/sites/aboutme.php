5c17d85471cea8c785f8a4c904bd00b1725d4c5a
Gogo Dev <gogomagnusdev@gmail.com>
2021-04-20 12:01:18 +0200

    footer formatting

--- LB2/sites/aboutme.php
+++ LB2/sites/aboutme.php
@@ -3,8 +3,9 @@
   ~ Created by Gottfried Stoll
   -->
 
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
 
@@ -21,104 +22,97 @@
     <meta name="theme-color" content="#ffffff">
 
     <!--Bootstrap JS-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
     <!--CSS-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/aboutme-style.css">
 
     <!--Title-->
     <title>GUP - About Me</title>
 </head>
+
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="../index.php">
-            <img class="logo" src="../img/logo/logo.jpg" alt="logo">
-        </a>
-        <span id="logo-text">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="../index.php">
+                <img class="logo" src="../img/logo/logo.jpg" alt="logo">
+            </a>
+            <span id="logo-text">
                 Gogo's Ultimate Pac-Man
-        </span>
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="../index.php">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">
-                        About Me
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./download.php">
-                        Download
-                    </a>
-                </li>
-            </ul>
+            </span>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php">
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            About Me
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./download.php">
+                            Download
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+    <main>
+        <div class="fixed-bg">
+            <h1>About Me</h1>
+        </div>
+        <div class="d-flex bd-highlight">
+            <div class="p-2 flex-shrink-1 bd-highlight">
+                <img src="../img/me.jpg" id="picofme" alt="Me">
+            </div>
+            <div class="p-2 w-100 bd-highlight">
+                <p>
+                    Hallo!<br>Mein Name ist Gottfried Stoll und ich bin Informatiker
+                    im dritten Lehrjahr.
+                    <br>Ich wollte unbedingt mal ein Spiel programmieren,
+                    und da Pac-Man ein Klassiker ist, und mir ausserdem noch gefällt, wollte
+                    ich es mal <br>selber ausprobieren. Ich hoffe dass es Ihnen gefällt, <br>denn ich habe
+                    mir sehr viel Mühe gegeben.
+                </p>
+            </div>
+
+
         </div>
 
-    </nav>
-</header>
-<main>
-    <div class="fixed-bg">
-        <h1>About Me</h1>
-    </div>
-    <div class="d-flex bd-highlight">
-        <div class="p-2 flex-shrink-1 bd-highlight">
-            <img src="../img/me.jpg" id="picofme" alt="Me">
-        </div>
-        <div class="p-2 w-100 bd-highlight">
+
+        <div class="align-self-baseline">
             <p>
-                Hallo!<br>Mein Name ist Gottfried Stoll und ich bin Informatiker
-                im dritten Lehrjahr.
-                <br>Ich wollte unbedingt mal ein Spiel programmieren,
-                und da Pac-Man ein Klassiker ist, und mir ausserdem noch gefällt, wollte
-                ich es mal <br>selber ausprobieren. Ich hoffe dass es Ihnen gefällt, <br>denn ich habe
-                mir sehr viel Mühe gegeben.
+                Wenn Sie zurück auf die Home-Page wollen, können Sie dies in dem Sie entweder, auf das Logo klicken,<br>
+                oder einfach auf Home klicken. Wenn Sie das Spiel herunterladen wollen, gehen Sie auf die
+                Download-Seite<br>
+                dort werden Sie dann weitergeführt.
             </p>
         </div>
+    </main>
+    <footer>
+        <div class="card text-center">
 
-
-    </div>
-
-
-    <div class="align-self-baseline">
-        <p>
-            Wenn Sie zurück auf die Home-Page wollen, können Sie dies in dem Sie entweder, auf das Logo klicken,<br>
-            oder einfach auf Home klicken. Wenn Sie das Spiel herunterladen wollen, gehen Sie auf die
-            Download-Seite<br>
-            dort werden Sie dann weitergeführt.
-        </p>
-    </div>
-</main>
-<footer>
-    <div class="card text-center">
-
-        <div class="card-header">
-            Gogo's Ultimate Pac-Man
+            <div class="card-header">
+                Gogo's Ultimate Pac-Man
+            </div>
+            <div class="card-footer text-muted">
+                This website was created by © Gottfried Stoll,
+                all rights reserved. <a href="./impressum.php">Impressum</a> | <a href="./datenschutz.php">Datenschutzerklärung</a>
+            </div>
         </div>
-        <div class="card-footer text-muted">
-            This website was created by © Gottfried Stoll,
-            all rights reserved. <a href="./impressum.php">Impressum</a> <a href="./datenschutz.php">Datenschutzerklärung</a>
-        </div>
-    </div>
-</footer>
+    </footer>
 </body>
+
 </html>
--- LB2/sites/download.php
+++ LB2/sites/download.php
@@ -107,7 +107,7 @@
             </div>
             <div class="card-footer text-muted">
                 This website was created by © Gottfried Stoll,
-                all rights reserved. <a href="./impressum.php">Impressum</a> <a href="./datenschutz.php">Datenschutzerklärung</a>
+                all rights reserved. <a href="./impressum.php">Impressum</a> | <a href="./datenschutz.php">Datenschutzerklärung</a>
             </div>
         </div>
     </footer>
